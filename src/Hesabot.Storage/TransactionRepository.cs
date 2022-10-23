614827cecffaf840f9b66a6481d201d06ac88272
imaun <imun22@gmail.com>
2022-10-23 23:37:41 +0330

    Add TransactionRepository

---
+++ src/Hesabot.Storage/TransactionRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hesabot.Core.Models;
+using Hesabot.Storage.Core;
+using Hesabot.Storage.Contracts;
+
+namespace Hesabot.Storage
+{
+
+    public class TransactionRepository : BaseRepository<Transaction, long>, ITransactionRepository
+    {
+
+        public TransactionRepository(NPoco.IDatabase db) : base(db) {
+        }
+    }
+}
