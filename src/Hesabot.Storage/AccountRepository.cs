f2c1d20563e0444f1759e55c7ad1b9478837fccb
imaun <imun22@gmail.com>
2022-10-23 23:33:13 +0330

    Add AccountRepository

---
+++ src/Hesabot.Storage/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hesabot.Storage.Core;
+using Hesabot.Core.Models;
+using Hesabot.Storage.Contracts;
+using NPoco;
+
+namespace Hesabot.Storage
+{
+    public class AccountRepository : BaseRepository<Account, long>, IAccountRepository
+    {
+        public AccountRepository(IDatabase db) : base(db) {
+        }
+
+        public Task<IReadOnlyCollection<Account>> GetUserAccountsAsync(long userId) {
+            throw new NotImplementedException();
+        }
+    }
+}
