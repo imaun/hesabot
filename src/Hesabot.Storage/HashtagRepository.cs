64d2b8e8cf47d1f0bbcec7f916e84308cce6e95b
imaun <imun22@gmail.com>
2022-10-23 23:34:28 +0330

    Add HashtagRepository

--- src/Hesabot.Storage/HashtagRepository.cs
+++ src/Hesabot.Storage/HashtagRepository.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hesabot.Core.Models;
+using Hesabot.Storage.Core;
+using Hesabot.Storage.Contracts;
+using NPoco;
 
 namespace Hesabot.Storage
 {
-    public class HashtagRepository
+    public class HashtagRepository : BaseRepository<Hashtag, long>, IHashtagRepository
     {
+        public HashtagRepository(IDatabase db) : base(db) {
+        }
+
+        public Task<bool> ExistAsync(long userId, string title) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hashtag> FindByTitleAsync(long userId, string title) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<Hashtag>> GetUserHashtagsAsync(long userId) {
+            throw new NotImplementedException();
+        }
     }
 }
