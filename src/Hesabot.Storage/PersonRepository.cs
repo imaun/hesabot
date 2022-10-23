3cb26662ca6f26ee7e790b6912c61d7feea9238a
imaun <imun22@gmail.com>
2022-10-23 23:36:17 +0330

    Add PersonRepository

---
+++ src/Hesabot.Storage/PersonRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hesabot.Core.Models;
+using Hesabot.Storage.Core;
+using Hesabot.Storage.Contracts;
+using NPoco;
+
+namespace Hesabot.Storage
+{
+    public class PersonRepository : BaseRepository<Person, long>, IPersonRepository
+    {
+        public PersonRepository(IDatabase db) : base(db) {
+        }
+
+        public Task<IReadOnlyCollection<Person>> GetUserPeaopleAsync(long userId) {
+            throw new NotImplementedException();
+        }
+    }
+}
