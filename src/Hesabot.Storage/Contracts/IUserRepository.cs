44a0370ae0197966dd45b31ef2af112459e06c5c
imaun <imun22@gmail.com>
2022-10-23 23:29:27 +0330

    Add User Repository contract

---
+++ src/Hesabot.Storage/Contracts/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hesabot.Storage.Core;
+using Hesabot.Core.Models;
+
+namespace Hesabot.Storage.Contracts
+{
+    public interface IUserRepository
+    {
+
+        Task<User> FindByTelegramUserNameAsync(string telegramUserName);
+    }
+}
